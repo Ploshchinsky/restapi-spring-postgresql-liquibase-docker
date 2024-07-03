a55991b802a454596585e14bc8f86f1c77930913
Anton Ploshchinsky <151565633+Ploshchinsky@users.noreply.github.com>
2024-07-03 15:54:36 +0500

    Delete src/main/java/ploton/restapi_postgres_docker/service directory

--- src/main/java/ploton/restapi_postgres_docker/service/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package ploton.restapi_postgres_docker.service;
-
-import org.springframework.data.repository.CrudRepository;
-import ploton.restapi_postgres_docker.model.Transaction;
-
-import java.util.UUID;
-
-public interface TransactionRepository extends CrudRepository<Transaction, UUID> {
-}
